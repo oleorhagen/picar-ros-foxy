3ba6fd101d0cadfaa67b9086bf5110e92349174b
Ole Petter <ole.orhagen@northern.tech>
2021-04-22 19:25:19 +0200

    Cleanup the Python Ackermann controller
    Changelog: None
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- pibot_pycontrol/pibot_pycontrol/motor_control.py
+++ pibot_pycontrol/pibot_pycontrol/motor_control.py
@@ -1,107 +1,81 @@
 import RPi.GPIO as GPIO
 
-# motor_EN_A: Pin7  |  motor_EN_B: Pin11
-# motor_A:  Pin8,Pin10    |  motor_B: Pin13,Pin12
+# motor_EN_B: Pin11
+#    |  motor_B: Pin13,Pin12
 
-# EN(able) motor A - Setting pin 4 high enables driver channel _
-Motor_A_EN = 4
 # EN(able) motor B - Setting pin 17 high enables driver channel _
 Motor_B_EN = 17
-
 # Motor driver inputs
 # Two control inputs for each motor
-Motor_A_Pin1 = 26
-Motor_A_Pin2 = 21
 Motor_B_Pin1 = 27
 Motor_B_Pin2 = 18
 
-Dir_forward = 0
-Dir_backward = 1
-
-left_forward = 1
-left_backward = 0
-
-right_forward = 0
-right_backward = 1
-
-pwn_A = 0
-pwm_B = 0
-
+# States
+stop = 1
+forward =  2
+backward = 3
 
 class Motor:
+    # motor_EN_A: Pin7  |
+    # motor_A:  Pin8,Pin10
+
+    state = stop
+
+    def __init__(self, enable_pin, pin1, pin2):
+        self.enable_pin = enable_pin
+        self.pin1 = pin1
+        self.pin2 = pin2
+        GPIO.setup(self.enable_pin, GPIO.OUT)
+        GPIO.setup(self.pin1, GPIO.OUT)
+        GPIO.setup(self.pin2, GPIO.OUT)
+        self.pwm = GPIO.PWM(self.enable_pin, 1000)
+
+    def forward(self, speed):
+        if self.state == forward:
+            self.pwm.ChangeDutyCycle(speed)
+            return
+        GPIO.output(self.pin1, GPIO.LOW)
+        GPIO.output(self.pin2, GPIO.HIGH)
+        self.pwm.start(0)
+        self.pwm.ChangeDutyCycle(speed)
+        self.state = forward
+
+    def backward(self, speed):
+        if self.state == backward:
+            self.pwm.ChangeDutyCycle(speed)
+            return
+        GPIO.output(self.pin1, GPIO.HIGH)
+        GPIO.output(self.pin2, GPIO.LOW)
+        self.pwm.start(0)
+        self.pwm.ChangeDutyCycle(speed)
+        self.state = backward
+
+    def stop(self, speed):
+        GPIO.output(self.self.pin1, GPIO.LOW)
+        GPIO.output(self.self.pin2, GPIO.LOW)
+        GPIO.output(self.enable_pin, GPIO.LOW)
+        self.state = stop
+
+
+class MotorController:
     """A simple wrapper class for controlling the PWM motor on the PiCar Pro"""
 
     def __init__(self):  # Motor initialization
-        global pwm_A, pwm_B
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(Motor_A_EN, GPIO.OUT)
-        GPIO.setup(Motor_B_EN, GPIO.OUT)
-        GPIO.setup(Motor_A_Pin1, GPIO.OUT)
-        GPIO.setup(Motor_A_Pin2, GPIO.OUT)
-        GPIO.setup(Motor_B_Pin1, GPIO.OUT)
-        GPIO.setup(Motor_B_Pin2, GPIO.OUT)
+        self.left = Motor(enable_pin=4, pin1=26, pin2=21)
+        self.right = Motor(enable_pin=17, pin1=27, pin2=18)
 
-        self.stop()
-        try:
-            pwm_A = GPIO.PWM(Motor_A_EN, 1000)
-            pwm_B = GPIO.PWM(Motor_B_EN, 1000)
-        except:
-            pass
+    def forward(self, speed):
+        if speed <= 0:
+            raise Exception("Trying to set a negative speed on a forward command")
+        self.left.forward(speed)
+        self.right.forward(speed)
 
-    def stop(self):  # Motor stops
-        GPIO.output(Motor_A_Pin1, GPIO.LOW)
-        GPIO.output(Motor_A_Pin2, GPIO.LOW)
-        GPIO.output(Motor_B_Pin1, GPIO.LOW)
-        GPIO.output(Motor_B_Pin2, GPIO.LOW)
-        GPIO.output(Motor_A_EN, GPIO.LOW)
-        GPIO.output(Motor_B_EN, GPIO.LOW)
-
-    def left(self, status, direction, speed):  # Motor 2 positive and negative rotation
-        if status == 0:  # stop
-            GPIO.output(Motor_B_Pin1, GPIO.LOW)
-            GPIO.output(Motor_B_Pin2, GPIO.LOW)
-            GPIO.output(Motor_B_EN, GPIO.LOW)
-        else:
-            if direction == Dir_backward:
-                GPIO.output(Motor_B_Pin1, GPIO.HIGH)
-                GPIO.output(Motor_B_Pin2, GPIO.LOW)
-                pwm_B.start(100)
-                pwm_B.ChangeDutyCycle(speed)
-            elif direction == Dir_forward:
-                GPIO.output(Motor_B_Pin1, GPIO.LOW)
-                GPIO.output(Motor_B_Pin2, GPIO.HIGH)
-                pwm_B.start(0)
-                pwm_B.ChangeDutyCycle(speed)
-
-    def right(self, status, direction, speed):  # Motor 1 positive and negative rotation
-        if status == 0:  # stop
-            GPIO.output(Motor_A_Pin1, GPIO.LOW)
-            GPIO.output(Motor_A_Pin2, GPIO.LOW)
-            GPIO.output(Motor_A_EN, GPIO.LOW)
-        else:
-            if direction == Dir_forward:  #
-                GPIO.output(Motor_A_Pin1, GPIO.HIGH)
-                GPIO.output(Motor_A_Pin2, GPIO.LOW)
-                pwm_A.start(100)
-                pwm_A.ChangeDutyCycle(speed)
-            elif direction == Dir_backward:
-                GPIO.output(Motor_A_Pin1, GPIO.LOW)
-                GPIO.output(Motor_A_Pin2, GPIO.HIGH)
-                pwm_A.start(0)
-                pwm_A.ChangeDutyCycle(speed)
-        return direction
-
-    def command(self, speed, angle):
-        """TODO: For now, simply ignore the steering angle, and have the wheels go the same speed"""
+    def backward(self, speed):
         if speed >= 0:
-            self.left(1, left_forward, speed)
-            self.right(1, right_forward, speed)
-        else:
-            # Negative speed
-            self.left(0, left_backward, speed)
-            self.right(0, right_backward, speed)
+            raise Exception("Trying to set a positive speed on a backward command")
+        self.left.backward(speed)
+        self.right.backward(speed)
 
-    def destroy():
-        self.stop()
-        GPIO.cleanup()  # Release resource
+    def stop(self):
+        self.left.stop()
+        self.right.stop()
